c355a5c51e8e5dac36be84e46feb649b56c5d18f
Marcelo Sampaio <marcelo@artlogica.com.br>
2017-03-01 16:21:02 -0300

    initil load

---
+++ SwiftPaperSwitch/ConfigTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ConfigTableViewController.swift
+//  SwiftPaperSwitch
+//
+//  Created by Marcelo Sampaio on 3/1/17.
+//  Copyright © 2017 Marcelo Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class ConfigTableViewController: UITableViewController {
+
+    // Outlets
+    
+
+    
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
